d66de12ca9fcfe66abcce32b1513b8cc85b4704b
a13576301231 <2963663242@qq.com>
2023-02-18 17:01:20 +0800

    完成了CFramelessWindow的构造函数

--- src/CFramelessWindow.cpp
+++ src/CFramelessWindow.cpp
@@ -1,7 +1,29 @@
 #include "CFramelessWindow.h"
-
+#include <Windows.h>
+#include <dwmapi.h>
 CFramelessWindow::CFramelessWindow(QWidget* parent):QMainWindow(parent) {
 	setWindowFlags(Qt::FramelessWindowHint|Qt::WindowMinimizeButtonHint|Qt::WindowMaximizeButtonHint);
+	QWidgetData * data = *((QWidgetData**)((byte *)this + 0x14));
+	bool visiable = (data->widget_attributes >> 0xF) & 0x1;
+	Qt::WindowFlags flags = data->window_flags;
+	HWND winid;
+	LONG windowLong;
+	MARGINS MarInset = { 1,1,1,1 };
+	if(m45){
+			setWindowFlags(flags | Qt::WindowMaximizeButtonHint);
+			winid = (HWND)winId();
+			windowLong = GetWindowLongW(winid, GWL_STYLE);
+			windowLong |=  WS_CAPTION | WS_SIZEBOX | WS_MAXIMIZEBOX;
+	}
+	else{
 
+		setWindowFlags((Qt::WindowFlags)(flags | 0x0FFFF7FFF));
+		winid = (HWND)winId();
+		windowLong = GetWindowLongW(winid, GWL_STYLE);
+		windowLong &= 0xFF3EFFFF;
+	}
+	SetWindowLongW(winid, GWL_STYLE, windowLong );
+	DwmExtendFrameIntoClientArea((HWND)winId(), &MarInset);
+	this->setVisible(visiable);
 
 }
--- src/CFramelessWindow.h
+++ src/CFramelessWindow.h
@@ -10,4 +10,12 @@ private:
 	int m20=5;
 	int m24=0;
 	int m28=0;
+	int m2c=0;
+	int m30=0;
+	int m34=0;
+	int m38=0;
+	int m3c=0;
+	int m40=0;
+	bool m44=0;
+	bool m45=1;
 };
